91c6030d7c6d59c7fa84a33c2a3505fc4374b43e
d0won <ldwlee03@gmail.com>
2024-11-08 00:50:36 +0000

    yolo-mnist 모델  추가

---
+++ yolo-mnist.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolo11n-cls.pt")  # load a pretrained model (recommended for training)
+
+# Train the model
+results = model.train(data="mnist", epochs=100, imgsz=3, device = "cuda")
